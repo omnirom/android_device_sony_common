38ffb7381d88f45367d99e6ba306346498737c52
Erik Castricum <erikcas1972@gmail.com>
2016-02-03 10:08:06 +0100

    common: simplify 32 bit custombootimg
    Change-Id: I3e812d32e4d59173dd7a1112019378dfd49cb55b
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- boot/custombootimg_32.mk
+++ boot/custombootimg_32.mk
@@ -13,22 +13,10 @@ DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
 	@echo -e ${CL_CYN}"----- Making DT image ------"${CL_RST}
 	$(call pretty,"Target DT image: $@")
-	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/dts/
+	$(hide) $(DTBTOOL) $(TARGET_DTB_EXTRA_FLAGS) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/dts/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
-$(INSTALLED_BOOTIMAGE_TARGET): \
-    $(PRODUCT_OUT)/kernel \
-    $(uncompressed_ramdisk) \
-    $(recovery_uncompressed_ramdisk) \
-    $(INSTALLED_RAMDISK_TARGET) \
-    $(INITSH) \
-    $(PRODUCT_OUT)/utilities/busybox \
-    $(PRODUCT_OUT)/utilities/extract_elf_ramdisk \
-    $(PRODUCT_OUT)/utilities/keycheck \
-    $(MKBOOTIMG) $(MINIGZIP) \
-    $(INTERNAL_BOOTIMAGE_FILES) \
-    $(INSTALLED_DTIMAGE_TARGET)
-
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(INITSH) $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/extract_elf_ramdisk $(PRODUCT_OUT)/utilities/keycheck $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
 	@echo -e ${CL_CYN}"----- Making boot image ------"${CL_RST}
 	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
@@ -47,25 +35,11 @@ $(INSTALLED_BOOTIMAGE_TARGET): \
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
 
-	$(hide) $(MKBOOTIMG) \
-        --kernel $(PRODUCT_OUT)/kernel \
-        --ramdisk $(PRODUCT_OUT)/combinedroot.fs \
-        --cmdline "$(BOARD_KERNEL_CMDLINE)" \
-        --base $(BOARD_KERNEL_BASE) \
-        --pagesize $(BOARD_KERNEL_PAGESIZE) \
-        --dt $(INSTALLED_DTIMAGE_TARGET) $(BOARD_MKBOOTIMG_ARGS) \
-        -o $(INSTALLED_BOOTIMAGE_TARGET)
+	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/combinedroot.fs --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --dt $(INSTALLED_DTIMAGE_TARGET) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_BOOTIMAGE_TARGET)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) $(INSTALLED_DTIMAGE_TARGET)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG) \
-        --kernel $(PRODUCT_OUT)/kernel \
-        --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img \
-        --cmdline "$(BOARD_KERNEL_CMDLINE)" \
-        --base $(BOARD_KERNEL_BASE) \
-        --pagesize $(BOARD_KERNEL_PAGESIZE) \
-        --dt $(INSTALLED_DTIMAGE_TARGET) $(BOARD_MKBOOTIMG_ARGS) \
-        -o $(INSTALLED_RECOVERYIMAGE_TARGET)
+	$(hide) $(MKBOOTIMG) --kernel $(PRODUCT_OUT)/kernel --ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --dt $(INSTALLED_DTIMAGE_TARGET) $(BOARD_MKBOOTIMG_ARGS) -o $(INSTALLED_RECOVERYIMAGE_TARGET)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
