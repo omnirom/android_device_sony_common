311244f446ac2ee6f1eba588f5d80aefa7805d2c
Erik Castricum <erikcas1972@gmail.com>
2016-02-03 10:06:44 +0100

    dtbtool / keycheck: update for M
    Change-Id: Iaab3bf59350807184df7fc79bf2b9e5b303d1fa9
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- extract_elf_ramdisk/Android.mk
+++ extract_elf_ramdisk/Android.mk
@@ -5,12 +5,13 @@ LOCAL_SRC_FILES := extract_elf_ramdisk.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libelf libc libm libz
 LOCAL_C_INCLUDES := \
-	external/elfutils/0.153/libelf \
+	external/elfutils/src/libelf \
 	external/zlib
 LOCAL_CFLAGS := -g -c -W
 LOCAL_MODULE := extract_elf_ramdisk
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_PACK_MODULE_RELOCATIONS := false
 include $(BUILD_EXECUTABLE)
 
