cf08cc8fb3f5002dd122826e79c660c8d78f0c2c
Erik Castricum <erikcas1972@gmail.com>
2016-02-03 09:47:25 +0100

    common: add omni configs
    Change-Id: Ie0fedecc5659d5ed7567db4de3b8e8982d878f63
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

---
+++ common_omni.mk
@@ -0,0 +1,35 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include common
+include device/sony/common/common.mk
+
+# Inclode custom boot
+include device/sony/common/omni_boot.mk
+
+# dtbtool
+PRODUCT_PACKAGES += \
+    dtbTool
+
+# Bootimg tools
+PRODUCT_PACKAGES += \
+    extract_elf_ramdisk
+
+# Recovery keycheck
+PRODUCT_PACKAGES += \
+    keycheck
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    telephony.lteOnGSMDevice=1
