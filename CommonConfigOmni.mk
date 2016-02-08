8efabd6dd977e875d48dfb4409fe29d84eea2cf3
Humberto Borba <humberos@gmail.com>
2016-02-07 22:42:56 -0200

    sony: common: Add TWRP common stuff
    Move TWRP common config from device to sony common repo.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ifc9e05fc4107cbbd4194015b8dfecb631d03cc87

--- CommonConfigOmni.mk
+++ CommonConfigOmni.mk
@@ -34,3 +34,29 @@ HEALTHD_ENABLE_TRICOLOR_LED := true
 RED_LED_PATH := /sys/class/leds/led:rgb_red/brightness
 GREEN_LED_PATH := /sys/class/leds/led:rgb_green/brightness
 BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
+
+# TWRP UI
+BOARD_HAS_NO_SELECT_BUTTON := true
+TW_NO_SCREEN_BLANK := true
+TW_MAX_BRIGHTNESS := 4095
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /sys/class/leds/wled:backlight/brightness
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+
+# TWRP Storage
+RECOVERY_SDCARD_ON_DATA := true
+TW_INCLUDE_FUSE_EXFAT := true
+TW_FLASH_FROM_STORAGE := true
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TARGET_NO_SEPARATE_RECOVERY := true
+
+# TWRP Crypto
+TW_INCLUDE_JB_CRYPTO := false
+TW_INCLUDE_L_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/bootdevice/by-name/userdata"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
