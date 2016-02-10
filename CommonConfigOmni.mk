38a42600b3ba937afccc1866ba355a95934b8f38
erikcas <erikcas1972@gmail.com>
2016-02-10 11:42:13 +0100

    common: TARGET_DTB_EXTRA_FLAGS is already defined optional
    Change-Id: I8dd309d5acd6019557a8773f5bd2ac3119efc84b
    Signed-off-by: erikcas <erikcas1972@gmail.com>

--- CommonConfigOmni.mk
+++ CommonConfigOmni.mk
@@ -23,7 +23,6 @@ TARGET_KERNEL_SOURCE := kernel/sony/msm
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/common/boot/custombootimg.mk
-TARGET_DTB_EXTRA_FLAGS := --force-v2
 ifneq ($(filter kitakami kanuti yukon,$(SOMC_PLATFORM)),)
     TARGET_DTB_EXTRA_FLAGS := -2
 endif
