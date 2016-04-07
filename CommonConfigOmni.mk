0cf569371fcf6a51ecc666fb67f9796ff9c091db
Erik Castricum <erikcas1972@gmail.com>
2016-04-08 00:43:05 +0200

    common: remove dtb references from custombootimg
    * only yukon platforms still need this
    * so tell config only to use this when not in yukon
    * yukon will have it's private custombootimg
    * see https://gerrit.omnirom.org/#/c/17594/ for yukon
    
    Change-Id: I045b8ee1d52c7058cbcdadc8e332d382998a62d1
    Signed-off-by: Erik Castricum <erikcas1972@gmail.com>

--- CommonConfigOmni.mk
+++ CommonConfigOmni.mk
@@ -23,10 +23,8 @@ SONY_AOSP ?= true
 TARGET_KERNEL_SOURCE := kernel/sony/msm
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DT := true
+ifneq ($(filter-out yukon,$(SOMC_PLATFORM),))
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/common/boot/custombootimg.mk
-TARGET_DTB_EXTRA_FLAGS := --force-v2
-ifneq ($(filter kitakami kanuti yukon,$(SOMC_PLATFORM)),)
-    TARGET_DTB_EXTRA_FLAGS := -2
 endif
 
 # Healthd
