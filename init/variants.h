022eeba9fda50281d81ca7352c67929f01bc4cef
Humberto Borba <humberos@gmail.com>
2016-06-15 17:22:29 -0300

    sony: common: Add init_msm for device variant detection
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I0e96a63ba15589fc5664ff447dc295e1fd889ad8

---
+++ init/variants.h
@@ -0,0 +1,42 @@
+// int variants [VARIANTS][3] = {modelNo, isDS, hasLTE,};
+
+int variants [24][3] = {
+//	Kitakami Devices
+//		Ivy
+		6553, 0, 1,
+//		Karin
+		771, 0, 1,
+//		Karin Windy
+		712, 0, 0,
+//		Sumire
+		6633, 1, 1,
+		6653, 0, 1,
+//	Rhine Devices
+//		Amami
+		5503, 0, 1,
+//		Honami
+		6902, 0, 1,
+		6903, 0, 1,
+//	Shinano Devices
+//		Aries
+		5803, 0, 1,
+		5833, 0, 1,
+//		Leo
+		6603, 0, 1,
+		6616, 0, 1,
+		6633, 1, 1,
+		6643, 0, 1,
+		6653, 0, 1,
+//		Sirius
+		6502, 0, 1,
+		6503, 0, 1,
+		6543, 0, 1,
+//	Yukon Devices
+//		Eagle
+		2302, 1, 0,
+		2303, 0, 1,
+		2305, 0, 0,
+		2306, 0, 1,
+		2403, 0, 1,
+		2406, 0, 1
+};
