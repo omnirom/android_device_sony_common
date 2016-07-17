573a1262f63f9429f0e4fa8f67af80c5ccf358f9
Humberto Borba <humberos@gmail.com>
2016-07-17 00:54:26 -0300

    sony: common: Move omni custom configuration to common
    Move common omni configuration to common repo then avoid
    unnecessary quantity of the same change on devices repos.
    
    DRY.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I3bf3871145ef6c6a6ff5bfb2bb3225a05b351013

--- CommonConfigOmni.mk
+++ CommonConfigOmni.mk
@@ -42,3 +42,11 @@ HEALTHD_ENABLE_TRICOLOR_LED := true
 RED_LED_PATH := /sys/class/leds/led:rgb_red/brightness
 GREEN_LED_PATH := /sys/class/leds/led:rgb_green/brightness
 BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
+
+# Inherit Omni GSM telephony parts
+ifneq ($(filter-out karin_windy,$(TARGET_DEVICE)),)
+    PRODUCT_PROPERTY_OVERRIDES += telephony.lteOnGSMDevice=1
+    include vendor/omni/config/gsm.mk
+endif
+
+include vendor/omni/config/common.mk
