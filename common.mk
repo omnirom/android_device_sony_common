9324ca149375266059df4c54eed2a5fa4b04ee09
Erik Castricum <git@cas-online.nl>
2017-02-22 07:19:29 +0100

    common: remove aosp camera properties

--- common.mk
+++ common.mk
@@ -273,31 +273,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nfc.port=I2C
 
-# Camera Debug
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.hal.debug=0 \
-    persist.camera.kpi.debug=0 \
-    persist.camera.global.debug=0
-
-# Disable Dual Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.dual.camera.mpo=0 \
-    persist.camera.dc.frame.sync=0 \
-    persist.camera.dcrf.enable=0
-
-# Camera Features
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.gyro.disable=1 \
-    persist.camera.stats.af.paaf=0 \
-    persist.camera.feature.cac=0 \
-    persist.camera.ois.disable=0 \
-    persist.camera.zsl.mode=1
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.HAL3.enabled=0 \
-    media.stagefright.less-secure=true \
-    media.stagefright.legacyencoder=true
-
 # Sensors debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.sensors.hal=0 \
