fcaeab1716c5e0475e34d3d395e3a8f53fc6e44b
Alexander Diewald <Diewi@diewald-net.com>
2017-03-06 21:18:40 +0100

    common: Rename & set rfkilldisabled property for brcm_fmradio.
    Rename rfkilldisabled to rfkilldisableduim in the parsing code
    of UIM such that we can set rfkilldisabled in the build.prop to
    stop android's bt implementation to mess around with the chip's
    power states.
    This allows to enable BT while the FMRadio is on.
    
    Change-Id: I6639e21ee4c1ca592ed70f417c825ed2a987be71
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- common.mk
+++ common.mk
@@ -308,3 +308,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # sdcardFS
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.sdcardfs=true
+
+# BT/FMRadio
+ifneq ($(filter shinano kitakami loire tone,$(PRODUCT_PLATFORM)),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rfkilldisabled=1
+endif
