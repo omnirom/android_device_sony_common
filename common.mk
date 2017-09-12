8605bf3c9ffd5aae2042a1802beac5c90eeb0c8f
Humberto Borba <humberos@gmail.com>
2017-09-12 16:24:07 -0300

    sony: common: Omnify
    Change-Id: I6bbb81ceb8a481410e31e9e191308a9ae4a2b3d9
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- common.mk
+++ common.mk
@@ -188,9 +188,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/system/lib/librqbalance.so
 
 # APN list
-PRODUCT_COPY_FILES += \
-    device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
-    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+#PRODUCT_COPY_FILES += \
+#    device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
+#    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
