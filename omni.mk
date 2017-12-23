349ad1e62f7626cb51d84f3e930cc270154c765c
Humberto Borba <humberos@gmail.com>
2017-12-23 16:38:08 -0200

    sony: common: Omnify
    Change-Id: I500a9b2ba6135c2009a01ec621042d80500a1530
    Signed-off-by: Humberto Borba <humberos@gmail.com>

---
+++ omni.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Sony AOSP Project
+SONY_AOSP ?= true
+
+# Common kernel source
+TARGET_KERNEL_SOURCE := kernel/sony/msm
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# SELinux
+BOARD_USE_ENFORCING_SELINUX := false
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# twrp
+$(call inherit-product, $(COMMON_PATH)/recovery/twrp.mk)
+
+# Omni config
+$(call inherit-product, vendor/omni/config/common.mk)
