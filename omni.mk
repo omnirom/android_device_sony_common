670e4525d996a7be5c447457b7003143e8625e4d
oshmoun <oshmoun100@googlemail.com>
2018-02-19 19:44:11 +0100

    omni.mk: add omni sepolicy
    Change-Id: Ie5f6dacebcfed0e0f5d724c4afb7b524ec9015c1

--- omni.mk
+++ omni.mk
@@ -30,6 +30,8 @@ TARGET_COMPILE_WITH_MSM_KERNEL := true
 BOARD_USE_ENFORCING_SELINUX := false
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
+BOARD_SEPOLICY_DIRS += \
+    vendor/omni/sepolicy
 
 # Healthd
 HEALTHD_FORCE_BACKLIGHT_CONTROL := true
