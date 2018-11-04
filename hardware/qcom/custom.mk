8a06e670d4c2210e2d31c913ca0340e8251a3bbf
Humberto Borba <humberos@omnirom.org>
2018-11-04 09:42:35 -0200

    sony: common: hardware: Use custom qcom hals makefile
    Prevent upstream merge conflicts.
    
    Signed-off-by: Humberto Borba <humberos@omnirom.org>
    Change-Id: I52dfebe81b8a2b861227b0618d09dd2779a82208

--- hardware/qcom/custom.mk
+++ hardware/qcom/custom.mk
@@ -1,6 +1,4 @@
-ifneq ($(wildcard device/sony/common/hardware/qcom/custom.mk),)
-    include device/sony/common/hardware/qcom/custom.mk
-else
+ifneq ($(CUSTOM_BUILD),)
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
 QCOM_BOARD_PLATFORMS += msm8952 msm8996 msm8998 sdm660 sdm845
@@ -11,26 +9,21 @@ MSM_VIDC_TARGET_LIST := msm8952 msm8996 msm8998 sdm660 sdm845
 #List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
-audio-hal := hardware/qcom/audio
-gps-hal := hardware/qcom/gps/sdm845
-ifeq ($(TARGET_USES_SDE),true)
-display-hal := hardware/qcom/display/sde
-QCOM_MEDIA_ROOT := hardware/qcom/media/sdm845
-else
-display-hal := hardware/qcom/display/msmfb
-QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
-endif
+audio-hal := hardware/sony/audio
+gps-hal := hardware/sony/gps/sdm845
+display-hal := hardware/sony/display
+QCOM_MEDIA_ROOT := hardware/sony/media/msm8998
 OMX_VIDEO_PATH := mm-video-v4l2
 media-hal := $(QCOM_MEDIA_ROOT)
 
-SRC_CAMERA_HAL_DIR ?= vendor/qcom/opensource/camera
+SRC_CAMERA_HAL_DIR ?= hardware/sony/camera
 SRC_DISPLAY_HAL_DIR := $(display-hal)
 SRC_MEDIA_HAL_DIR := $(QCOM_MEDIA_ROOT)
 TARGET_KERNEL_VERSION := $(SOMC_KERNEL_VERSION)
 
 include device/sony/common/hardware/qcom/utils.mk
 
-include $(display-hal)/Android.mk
+#include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
 include $(call all-makefiles-under,$(gps-hal))
 include $(call all-makefiles-under,$(media-hal))
