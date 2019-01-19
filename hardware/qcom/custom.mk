4cab384448448deb8f6a4dded230cc73fabeedd1
oshmoun <oshmoun100@googlemail.com>
2019-01-19 14:04:48 +0100

    sony: common: hardware: add ipa hal
    Change-Id: Ib683603ffc17c2419fdf736eda15501971b63ce4

--- hardware/qcom/custom.mk
+++ hardware/qcom/custom.mk
@@ -11,6 +11,7 @@ MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
 audio-hal := hardware/sony/audio
 gps-hal := hardware/sony/gps/sdm845
+ipa-hal := hardware/qcom/sdm845/data/ipacfg-mgr
 
 ifeq ($(TARGET_USES_SDE),true)
 display-hal := hardware/sony/display-sde
@@ -36,6 +37,7 @@ include device/sony/common/hardware/qcom/utils.mk
 
 #include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
+include $(call first-makefiles-under,$(ipa-hal))
 include $(call all-makefiles-under,$(gps-hal))
 include $(call all-makefiles-under,$(media-hal))
 endif
