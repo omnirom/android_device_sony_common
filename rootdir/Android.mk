19d763b27d1530ad17e82a216c2dd449c2491834
Felix <google@ix5.org>
2019-01-22 10:35:00 +0100

    HACK: neversleep: A utility to disable deep sleep
    Because of a prevalent bug concerning suspending to deep sleep on
    certain Sony platforms, introduce a workaround.
    
    Hold an infinite wakelock so the device never enters deep sleep and thus
    has no problems waking up in a timely manner.
    
    Because this is draining battery, introduce a property to enable or
    disable this behaviour.
    
    Change-Id: Idf7794725b2667b6396bb54803244c733398f9ee
    Signed-off-by: Felix <google@ix5.org>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -52,6 +52,15 @@ LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := neversleep.sh
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES_arm64 := vendor/bin/neversleep.sh
+LOCAL_INIT_RC_64  := vendor/etc/init/neversleep.rc
+LOCAL_MODULE_TARGET_ARCH := arm64
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := init.recovery.$(TARGET_DEVICE)
 LOCAL_SRC_FILES := init.recovery.common.rc
 LOCAL_MODULE_TAGS := optional
